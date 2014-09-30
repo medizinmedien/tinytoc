869dcf66e609593cd4b56df6eb24277296cfee0a
Frank Stuerzebecher <frank@hostz.at>
2014-09-30 22:11:31 +0200

    Grundlegende Anpassung an mO. Bugfixes, erste Optimierungen.

---
+++ CHANGES.md
@@ -0,0 +1,13 @@
+### Änderungen: TinyTOC (seitenbezogenes Inhaltsverzeichnis) für medONLINE
+
+#### Version 0.3.1 // 29./30.09.2014
+
+* Performance: Widget-Include aufgelassen.
+* Usability: automatische Rücksprungmarken von Überschriften zum Inhaltsverzeichnis.
+* Bugfix: Behebung vestümmelter Umlaute in allen Content-Bereichen.
+* Performance: Laden der Lokalisierung aufgelassen.
+* Issue auf mO: Unverträglichkeit mit unbekanntem Plugin mit niedrigerer Priorität behoben.
+
+#### Version 0.3
+
+* Fork des Original-Plugins
--- tiny_toc.php
+++ tiny_toc.php
@@ -1,13 +1,13 @@
 <?php
 /*
-Plugin Name: tinyTOC
-Plugin URI: http://wp.tribuna.lt/tiny-toc
-Description: Automaticly builds a Table of Contents once specific number (eg. 3) of headings (h1-h6) is reached and inserts it before or after post/page content
-Version: 0.3
+Plugin Name: tinyTOC for medONLINE
+Plugin URI: https://github.com/medizinmedien/tinytoc
+GitHub Plugin URI: https://github.com/medizinmedien/tinytoc
+Description: Bildet ein Inhaltsverzeichnis (TOC) auf Seiten und Beiträgen ab einer konfigurierbaren Anzahl (z.B. 2) von Überschriften (h1-h6) mittels Shortcode <code>[toc]</code>.
+Version: 0.3.1
 Author: Arūnas
-Author URI: http://wp.tribuna.lt/
+Author URI: http://wp.tribuna.lt/tiny-toc
 License: GPLv2 or later
-Text Domain: tiny_toc
 */
 // Make sure we don't expose any info if called directly
 if ( !function_exists( 'add_action' ) ) {
@@ -16,14 +16,9 @@ if ( !function_exists( 'add_action' ) ) {
 }
 
 //==========================================================
-// init textdomain
-load_plugin_textdomain( 'tiny_toc', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/');
-load_muplugin_textdomain( 'tiny_toc', dirname( plugin_basename( __FILE__ ) ) . '/languages/');
-
-//==========================================================
 // load associated files
 require_once(plugin_dir_path( __FILE__ ).'tiny_options.php');
-require_once(plugin_dir_path( __FILE__ ).'tiny_widget.php');
+//FS: not needed - require_once(plugin_dir_path( __FILE__ ).'tiny_widget.php');
 
 // init tinyConfiguration
 $tiny_toc_options = new tiny_toc_options(
@@ -79,8 +74,11 @@ register_activation_hook(__FILE__, array($tiny_toc_options,'add_defaults'));
 add_action('admin_init', array($tiny_toc_options,'init') );
 add_action('admin_menu', array($tiny_toc_options,'add_page'));
 
+// FS: Prio "100" (last) replaced with "7" (load first!) - otherwise collision
+// with an yet unknown plugin (TOC won't be added to content then, and headings
+// are not prepared with id's as link anchors):
+add_filter( 'the_content', array('tiny_toc','filter'), 7 );
 
-add_filter( 'the_content', array('tiny_toc','filter'), 100);
 add_shortcode( 'toc', array('tiny_toc','shortcode'));
 function get_toc($attr=array()) {return tiny_toc::template($attr);}
 function the_toc($attr=array()) {echo tiny_toc::template($attr);}
@@ -95,12 +93,12 @@ class tiny_toc {
   static function shortcode($attr,$content=false) {
     global $post, $tiny_toc_options;
     $min = (isset($attr['min'])&&$attr['min']>0)?$attr['min']:$tiny_toc_options->values['min'];
-    $toc = tiny_toc::create($post->post_content,$min);
+    $toc = tiny_toc::create( $post->post_content, $min );
     return $toc;
   }
-  static function filter($content) {
+  static function filter( $content ) {
     global $tiny_toc_options;
-    $toc = tiny_toc::create($content,$tiny_toc_options->values['min']);
+    $toc = tiny_toc::create( $content, $tiny_toc_options->values['min'] );
     if ($tiny_toc_options->values['position']=='above') {
       $content = $toc.$content;
     } elseif ($tiny_toc_options->values['position']=='below') {
@@ -139,7 +137,8 @@ class tiny_toc {
   }
 
   static function parse(&$content) {
-    $content = '<html><body>'.($content).'</body></html>';
+    // FS: Add UTF-8 support and prevent garbled umlauts.
+    $content = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /></head><body>'.($content).'</body></html>';
     $dom = new DOMDocument();
     libxml_use_internal_errors(true);
     $dom->loadHTML($content);
@@ -149,7 +148,7 @@ class tiny_toc {
     $items = array();
     $min_depth = 6;
     $parent = array();
-    for($i=0;$i<$tags->length;++$i) {
+    for( $i=0; $i < $tags->length; ++$i ) {
       $id = $tags->item($i)->getAttribute('id');
       if(!$id) {
         $id = 'h'.$i;
@@ -170,7 +169,7 @@ class tiny_toc {
     }
     $text = $xpath->query('/html/body');
     $text = $dom->saveHTML($text->item(0));
-    $content = $text;
+    $content = $text; // FS: Hier wird der Content ueberschrieben!
     return $items;
   }
 
@@ -180,7 +179,7 @@ class tiny_toc {
     if (sizeof($items)>=$min) {
       $walker = new tiny_toc_walker();
       $output = $walker->walk($items,0);
-      $output = "<nav class=\"tiny_toc\">\n<ol>\n{$output}</ol>\n</nav>\n\n";
+      $output = "<nav class=\"tiny_toc\">\n<h4 id=\"tinytoc\">Inhalt</h4>\n<ol>\n{$output}</ol>\n</nav>\n\n";
     }
     return $output;
   }
@@ -209,4 +208,34 @@ class tiny_toc_walker extends Walker {
   }
 }
 
-?>
+/**
+ * Set on-the-fly backlinks from headings to tinyTOC when present in content.
+ *
+ * FS - 30.09.2014 - frank@hostz.at
+ */
+add_action( 'wp_footer', 'tiny_toc_add_backlinks_to_headings' );
+function tiny_toc_add_backlinks_to_headings() {
+	global $post;
+	if( is_singular() && ( strpos( $post->post_content, '[toc]' ) !== false || strpos( $post->post_content, '#tinytoc') ) ) {
+		$backlink = '<span class="backlink2toc"><a href="#tinytoc">&#10548;</a></span>';
+		$backlink = apply_filters( 'tiny_toc_backlink', $backlink );
+
+		$output  = '<script type="text/javascript" id="tiny_toc">';
+		$output .=   'jQuery(document).ready(function($){';
+
+		$output .=     'headers=$("h2[id^=h],h3[id^=h],h4[id^=h],h5[id^=h],h6[id^=h]");';
+		$output .=     'is_toc=$("#tinytoc").size()>0;';
+
+		$output .=     'if(is_toc && headers.size()>0){';
+		$output .=       'headers.each(function(){';
+		$output .=         "$(this).append('$backlink')";
+		$output .=       '});';
+		$output .=     '}';
+
+		$output .=   '});';
+		$output .= '</script>';
+
+		echo $output;
+	}
+}
+
